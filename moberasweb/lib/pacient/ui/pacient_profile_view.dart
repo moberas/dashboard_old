80ec796e5b47639dfad55515bd9db732a3b430d5
LucasNGOmega27 <67060665+LucasNGOmega27@users.noreply.github.com>
2020-08-11 14:05:06 -0300

    mudança layout
    mudança (pequena) no layout

--- moberasweb/lib/pacient/ui/pacient_profile_view.dart
+++ moberasweb/lib/pacient/ui/pacient_profile_view.dart
@@ -6,38 +6,31 @@ class PacientProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<PacientProfileViewModel>.reactive(
-        viewModelBuilder: () => PacientProfileViewModel(),
-        builder: (context, model, child) => Scaffold(
-              body: Scrollbar(
-                child: SingleChildScrollView(
-                  padding: EdgeInsets.all(25),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            ...[
-                              TextField(
-                                controller: model.nameController,
-                                keyboardType: TextInputType.name,
-                                enableSuggestions: true,
-                                decoration: InputDecoration(labelText: 'Nome'),
-                              ),
-                              TextField(
-                                controller: model.cpfController,
-                                keyboardType: TextInputType.numberWithOptions(
-                                    signed: false, decimal: false),
-                                enableSuggestions: true,
-                                decoration: InputDecoration(labelText: 'CPF'),
-                              ),
-                            ]
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+      viewModelBuilder: () => PacientProfileViewModel(),
+      builder: (context, model, child) => Container(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.red,
               ),
-            ));
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            /*Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.black12,
+              ),
+            ),*/
+          ],
+        ),
+      ),
+    );
   }
 }
