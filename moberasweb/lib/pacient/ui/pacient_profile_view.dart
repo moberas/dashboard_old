5837c30c0f6a9a154e62e1fc3e10bb6bd5fe0b3d
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2020-08-13 17:01:06 -0300

    Theme Button

--- moberasweb/lib/pacient/ui/pacient_profile_view.dart
+++ moberasweb/lib/pacient/ui/pacient_profile_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moberasweb/commons/ui/Constants.dart';
 import 'package:moberasweb/pacient/ui/get_map_widget.dart';
 import 'package:moberasweb/pacient/ui/pacient_profile_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -8,50 +9,84 @@ class PacientProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<PacientProfileViewModel>.reactive(
       viewModelBuilder: () => PacientProfileViewModel(),
-      builder: (context, model, child) => Container(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
+      builder: (context, model, child) => Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Color(0xFFFFFFFF),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.black87,
+              ),
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context) {
+                return Constants.choices.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
                   color: Colors.red,
                   child: Padding(
-                    padding:
-                        const EdgeInsets.fromLTRB(500.0, 10.0, 500.0, 10.0),
+                    padding: const EdgeInsets.all(8.0),
                     child: Container(
                       child: getMap(),
                     ),
-                  )),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-              ),
-            ),
-            OutlineButton(
-              shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(30.0)),
-              color: Colors.green[300],
-              highlightedBorderColor: Colors.green[300],
-              onPressed: () => model.returntoview(),
-              child: new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Expanded(
-                    child: Text(
-                      "Voltar A Tela De Pesquisa",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.black87, fontWeight: FontWeight.bold),
-                    ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.blue,
+                ),
+              ),
+              OutlineButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                color: Colors.green[300],
+                highlightedBorderColor: Colors.green[300],
+                onPressed: () => model.returntoview(),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Expanded(
+                      child: Text(
+                        "Voltar A Tela De Pesquisa",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black87, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void choiceAction(String choice) {
+    if (choice == Constants.Child) {
+      print('Infantil');
+    } else if (choice == Constants.Adult) {
+      print('Adulto');
+    } else if (choice == Constants.Elder) {
+      print('Idoso');
+    } else if (choice == Constants.ColorBlind) {
+      print('Daltonico');
+    }
   }
 }
